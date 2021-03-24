8adabea3cacbadaab547881602ed4a3f7679ae4f
ian <ian.miell@container-solutions.com>
2021-03-24 18:26:39 +0000

    s

--- aws/terraform/main.tf
+++ aws/terraform/main.tf
@@ -61,7 +61,7 @@ resource "aws_spot_instance_request" "photogrammetry" {
   ami                             = "ami-0023ffc015ca50978" # Microsoft Windows Server 2016 Locale English with Nvidia GPU Grid Driver AMI provided by Amazon
   #instance_type                   = "t2.micro"              # for testing (not g instance)
   #instance_type                   = "g3s.xlarge"            # for slow, cheap testing (g instance)
-  instance_type                   = "g3s.4xlarge"            # fast g instance
+  instance_type                   = "g3.4xlarge"            # fast g instance
   spot_type                       = "one-time"
   associate_public_ip_address     = true
   wait_for_fulfillment            = true
